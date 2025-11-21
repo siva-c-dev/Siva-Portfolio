84f50650c70c6c6161e9c5377a9313c81c1f672a
siva-c-dev <sivagreenheart@gmail.com>
2025-11-21 10:15:05 +0530

    design correction

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -50,9 +50,9 @@ const Contact = () => {
         </div>
 
         <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ duration: 1.5 }}
+          initial={{ opacity: 0, scale: 0.8 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
           viewport={{ once: false, amount: 0.2 }}
         >
           <div className="address text-center">
